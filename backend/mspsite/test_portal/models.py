a18e44eb859294e7d33d08b74af7999780ac37c4
surbhigoel191 <44471561+surbhigoel191@users.noreply.github.com>
2019-06-19 20:14:31 +0530

    Update models.py

--- backend/mspsite/test_portal/models.py
+++ backend/mspsite/test_portal/models.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import User
 class Candidate(models.Model):
     firstname = models.CharField(max_length=25)
     lastname = models.CharField(max_length=25)
-    bitsmail = models.URLField(default='')
+    email = models.URLField(default='')
     bitsid = models.CharField(max_length=20)
     contact = models.IntegerField(default=0,min_length=10,max_length=10)
 
-def update_user_profile(sender, instance, created, **kwargs):
-   	if created:
-        Candidate.objects.create(user=instance)
-    	  instance.Candidate.save()
+    def update_user_profile(sender, instance, created, **kwargs):
+   	    if created:
+            Candidate.objects.create(user=instance)
+    	    instance.Candidate.save()
 # Create your models here.
