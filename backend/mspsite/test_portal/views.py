8057fe126f380e6e8d80d2df1a682ac67987e229
surbhigoel191 <44471561+surbhigoel191@users.noreply.github.com>
2019-06-19 20:21:35 +0530

    Update views.py

--- backend/mspsite/test_portal/views.py
+++ backend/mspsite/test_portal/views.py
@@ -3,21 +3,21 @@ from .models import Candidate
 from django.contrib.auth import login, authenticate
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
+from .forms import PostForm
 # Create your views here.
 def login(request):
 	if request.method == 'POST':
-        	form = Candidate(request.POST)
+        	form = PostForm(request.POST)
         	try:
-    			validate_email(bitsmail)
+    			validate_email(email)
 		except ValidationError:
     			print "oops! wrong email"
-		else:
-        		if form.is_valid():
+		if form.is_valid():
         		form.save()
                 	login(request, user)
                 	return redirect('test')
     	else:
-        	form = Candidate()
+        	form = PostForm()
     	return render(request, 'test_portal/login.html', {'form': form})
 def welcome(request):
 	return render(request,'test_portal/welcome.html')
