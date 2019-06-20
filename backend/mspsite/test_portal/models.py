4f78fbca41f5597bd2c1cc474ad830ac8a9f3bc8
ayushjain1144 <ayushjain1144@gmail.com>
2019-06-20 06:58:18 +0530

    fixed validation errors by surbhi

--- backend/mspsite/test_portal/models.py
+++ backend/mspsite/test_portal/models.py
@@ -1,18 +1,21 @@
 from django.db import models
-
+from django.conf import settings
 from django.utils import timezone
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-class Candidate(models.Model):
-    firstname = models.CharField(max_length=25)
-    lastname = models.CharField(max_length=25)
-    email = models.URLField(default='')
-    bitsid = models.CharField(max_length=20)
-    contact = models.IntegerField(default=0,min_length=10,max_length=10)
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-    def update_user_profile(sender, instance, created, **kwargs):
-   	    if created:
-            Candidate.objects.create(user=instance)
-    	    instance.Candidate.save()
+class Candidate(models.Model):
+	firstname = models.CharField(max_length=25)
+	lastname = models.CharField(max_length=25)
+	email = models.URLField(default='')
+	bitsid = models.CharField(max_length=20)
+	contact = models.IntegerField(default=0,validators=[MinValueValidator(100000000),MaxValueValidator(9999999999)])
+	use_required_attribute = True
+
+	def update_user_profile(sender, instance, created, **kwargs):
+		if created:
+			Candidate.objects.create(user=instance)
+			instance.Candidate.save()
 
 # Create your models here.
--- backend/mspsite/test_portal/views.py
+++ backend/mspsite/test_portal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Candidate
 from django.contrib.auth import login, authenticate
 from django.core.exceptions import ValidationError
@@ -7,18 +7,18 @@ from .forms import PostForm
 # Create your views here.
 def login(request):
 	if request.method == 'POST':
-        	form = PostForm(request.POST)
-        	try:
-    			validate_email(email)
+		form = PostForm(request.POST)
+		try:
+			validate_email(email)
 		except ValidationError:
-    			print "oops! wrong email"
+			print("oops! wrong email")
 		if form.is_valid():
-        		form.save()
-                	login(request, user)
-                	return redirect('test')
-    	else:
-        	form = PostForm()
-    	return render(request, 'test_portal/login.html', {'form': form})
+			form.save()
+			login(request, user)
+			return redirect('test')
+	else:
+		form = PostForm()
+	return render(request, 'test_portal/login.html', {'form': form})
 def welcome(request):
 	return render(request,'test_portal/welcome.html')
 def test(request):
