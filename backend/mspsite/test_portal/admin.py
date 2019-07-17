4fb37776a4a7a779cf1e0c426c26c3584167f232
surbhigoel191 <44471561+surbhigoel191@users.noreply.github.com>
2019-07-17 13:53:46 +0530

    Update admin.py

--- backend/mspsite/test_portal/admin.py
+++ backend/mspsite/test_portal/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 
 
 # Register your models here.
-from .models import Candidate, Question, Response, Questionm, Questioni
+from .models import Candidate, Question, Response, Questionm, Questioni, Responsem, Responsei
 
 admin.site.register(Questionm)
 admin.site.register(Questioni)
 admin.site.register(Question)
 admin.site.register(Candidate)
 admin.site.register(Response)
-
+admin.site.register(Responsem)
+admin.site.register(Responsei)
