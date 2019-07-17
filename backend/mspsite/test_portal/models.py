a4a75b0b81673a585fb6815c060b6c84bcbbb15f
surbhigoel191 <44471561+surbhigoel191@users.noreply.github.com>
2019-07-17 12:43:13 +0530

    Update models.py

--- backend/mspsite/test_portal/models.py
+++ backend/mspsite/test_portal/models.py
@@ -24,28 +24,36 @@ class Candidate(models.Model):
 	def __str__(self):
 		return self.bitsid
 
-class Question(models.Model):
-
-	question = models.CharField(max_length=256, default = '')
-	marks = models.IntegerField(default = 10)
-	id = models.AutoField(primary_key = True)
-
-	def __str__(self):
-		return self.question
-
-
-class Response(models.Model):
+class Responsei(models.Model):
 
 	user = models.ForeignKey("Candidate", on_delete=models.CASCADE,)
-	question = models.ForeignKey("Question", on_delete=models.CASCADE,)
+	question = models.ForeignKey("Questioni", on_delete=models.CASCADE,)
 
-	free_response = models.TextField(max_length = 2000, blank = True)
+	responsei = models.IntegerField(default = 10)
 
 	def submit(self):
 		self.save()
 
 	def __str__(self):
-		return self.free_response
+		return self.responsei
+	
+class Responsem(models.Model):
+
+	user = models.ForeignKey("Candidate", on_delete=models.CASCADE,)
+	question = models.ForeignKey("Questionm", on_delete=models.CASCADE,)
+
+	responsem = models.CharField(default='', max_length=200)
+
+	def submit(self):
+		self.save()
+
+	def __str__(self):
+		return self.responsem
+	
+class QuizAnswer(models.Model):
+    answer_option = models.ForeignKey(Responsei)
+    user = models.ForeignKey(Candidate)
+	
 class Questionm(models.Model):
 
 	question = models.TextField()
@@ -65,7 +73,6 @@ class Questionm(models.Model):
 class Questioni(models.Model):
 
 	question = models.TextField()
-	ans = models.IntegerField(default=0, max_length=200)
 	marks = models.IntegerField(default = 10)
 
 	def __str__(self):
