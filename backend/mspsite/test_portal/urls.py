f3f34b0c1bbc01646b0cdbdb0a6797d926aabdc8
Vaibhav Garg <gargvaibhav64@gmail.com>
2019-07-22 15:11:05 +0530

    Fixed Round 1 Display

--- backend/mspsite/test_portal/urls.py
+++ backend/mspsite/test_portal/urls.py
@@ -5,13 +5,18 @@ urlpatterns = [
     path('logout', views.logout, name='test_logout'),
     path('register', views.register, name = 'register'),
     path('', views.welcome, name='welcome'),
+
     path('question/<slug:id>/', views.question_list, name='question_list'),
     path('question/<slug:id>/<int:ques_no>/', views.question_list, name='question_list'),
+
     path('response_save/<int:pk>/<slug:id>/$', views.response_save, name='response_save'),
+    path('response_save/<int:pk>/<slug:id>/<int:next>/$', views.response_save, name='response_save'),
+
+    path('response_savem/<int:pk>/<slug:id>/<int:next>/$', views.response_savem, name='response_savem'),
     path('response_savem/<int:pk>/<slug:id>/$', views.response_savem, name='response_savem'),
-    path('response_savei/<int:pk>/<slug:id>/$', views.response_savei, name='response_savei'),
-    path('ques_detail/<slug:id>/<int:pk>/',views.ques_detail,name='ques_detail'),
-    path('ques_detail2/<slug:id>/<int:pk>/',views.ques_detail2,name='ques_detail2'),
+
+    path('ques_detail_mcq/<slug:id>/',views.ques_detail_mcq,name='ques_detail_mcq'),
+    path('ques_detail_mcq/<slug:id>/<int:ques_no>/',views.ques_detail_mcq,name='ques_detail_mcq'),
     path('round2', views.round2, name='round2'),
     path('proceed', views.proceed, name='proceed'),
 ]
